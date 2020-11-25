1513a85d6c54d15b2d5844b64626548645534298
byciitp <byciitp@gmail.com>
2020-11-25 21:01:48 +0530

    add images to footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,9 +2,9 @@ var links = document.querySelectorAll(".icon");
 
 for (var i = 0; i < links.length; i++) {
 	links[i].addEventListener("mouseover",function(event){
-		this.style.fontSize = "3em";
+		this.style.fontSize = "3.5em";
 	});
 	links[i].addEventListener("mouseout", function( event ){
-		this.style.fontSize = "2.5em";
+		this.style.fontSize = "3em";
 	});
 }
